5de5b35f06aa361a368b19b8fd274097e71b44b8
Waffe <kools23@gmail.com>
2016-09-01 18:07:46 +1000

    Menu Header

--- src/MENU/Menu.java
+++ src/MENU/Menu.java
@@ -4,6 +4,24 @@ package MENU;
  * Created by Martin on 1/09/2016.
  */
 public class Menu {
-    
+
+    private void printHeader(){
+
+        System.out.println("\n+--------------------------------+");
+        System.out.println("|          Study Progress        |");
+        System.out.println("|             System             |");
+        System.out.println("+--------------------------------+");
+
+    }
+
+    public void runMenu(){
+        printHeader();
+
+
+    }
+
+
+
+
 
 }
