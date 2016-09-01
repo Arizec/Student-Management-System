5d4c75fb2376efcebec5051a63d94093ee12fc80
Arizec <salufie@gmail.com>
2016-09-01 19:07:53 +1000

    Update Admin.java

--- src/USERS/Admin.java
+++ src/USERS/Admin.java
@@ -2,9 +2,12 @@ package USERS;
 
 /**
  * Created by Martin on 1/09/2016.
- * TESTING FETCH FUNCTION
  */
 public class Admin extends Staff {
   
-  public void createProgram();
+  public void createProgram(){
+	  
+  }
+  
 }
+
