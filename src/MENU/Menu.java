43a301ce3f96ae9dd19dd22e00f032fd65c1ba75
Waffe <kools23@gmail.com>
2016-09-01 18:30:05 +1000

    Menu

--- src/MENU/Menu.java
+++ src/MENU/Menu.java
@@ -1,9 +1,12 @@
 package MENU;
 
+import java.util.*;
+
 /**
  * Created by Martin on 1/09/2016.
  */
 public class Menu {
+
 
     private void printHeader(){
 
@@ -14,8 +17,62 @@ public class Menu {
 
     }
 
+    private void menuOptions(){
+        System.out.println("\n1. Set Up New Program");
+        System.out.println("2. Add Student Account(s)");
+        System.out.println("3. Upload Enrolment(s)");
+    }
+
+    private void performChoices(int choice){
+        switch (choice){
+            case 1:
+                System.out.println("1");
+                break;
+            case 2:
+                System.out.println("2");
+                break;
+            case 3:
+                System.out.println("3");
+                break;
+            default:
+                System.out.println("4");
+                break;
+
+
+
+        }
+
+
+    }
+
+
+    private int getInput(){
+        int choice = -1;
+        Scanner input = new Scanner(System.in);
+
+        while( choice <0 || choice > 3){										// catches exception if not a number between 0 and 12.
+            try {
+                System.out.print("\nEnter Your Choice: ");
+                choice = Integer.parseInt(input.nextLine());
+            }
+            catch(NumberFormatException e){
+                System.out.println("Invalid Selection. Please Try Again");
+            }
+        }
+        return choice;
+    }
+
+
+
+
+
+
     public void runMenu(){
+
         printHeader();
+        menuOptions();
+        int choice = getInput();
+        performChoices(choice);
 
 
     }
