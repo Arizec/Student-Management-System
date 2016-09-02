7fec2c6ee2c91b4e67600064c340c26942fdf215
Arizec <salufie@gmail.com>
2016-09-02 12:23:10 +1000

    Update Admin.java

--- src/USERS/Admin.java
+++ src/USERS/Admin.java
@@ -10,5 +10,30 @@ public class Admin extends Staff {
 	  
   }
   
+  public void createStudentID(){
+  	try
+		{
+
+		    String filename= "loginDetails.txt";
+		    FileWriter fw = new FileWriter(filename,true); //the true will append the new data
+		    Scanner reader = new Scanner(System.in);
+		  
+		    
+		    System.out.println("Create Student ID");
+		    String studentID = reader.nextLine();
+		    System.out.println("Create Student Password");
+		    String password = reader.nextLine();
+		    
+		    String logindetails = studentId + ":" + password;
+		   
+		    fw.write(addStudentID);//appends the string to the file
+		    fw.close();
+		}
+		catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+}
+  }
+  
 }
 
