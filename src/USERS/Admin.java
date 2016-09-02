f54bb70ee7e7a29b83835c1f5f50fa0a79144798
Arizec <salufie@gmail.com>
2016-09-02 12:46:12 +1000

    Update Admin.java

--- src/USERS/Admin.java
+++ src/USERS/Admin.java
@@ -38,6 +38,42 @@ public class Admin extends Staff {
 		    System.err.println("IOException: " + ioe.getMessage());
         }
   }
+  
+  public void createProgram(){
+  	try
+		{
+
+		    String filename= "programs.txt";
+		    FileWriter fw = new FileWriter(filename,true); //the true will append the new data
+		    Scanner reader = new Scanner(System.in);
+
+		    System.out.println("Creating a new program")
+		    
+		    System.out.println("Program code");
+		    String programCode = reader.nextLine();
+		    
+		    System.out.println("Version number");
+		    String versionNo = reader.nextLine();
+		    
+		    System.out.println("Credits to complete program");
+		    String creditsNeeded = reader.nextLine();
+		    
+		    System.out.println("Program type(Bachelor/Honours/Diploma/Masters)");
+		    String programType = reader.nextLine();
+		    
+		    System.out.println("Program status");
+		    String programStatus = reader.nextLine();
+		    
+		    String program = programCode + ":" + versionNo + ":" + creditsNeeded + ":" + programType + + ":" + programStatus;
+		   
+		    fw.write(program);//appends the string to the file
+		    fw.close();
+		}
+		catch(IOException ioe)
+		{
+		    System.err.println("IOException: " + ioe.getMessage());
+        }
+  }
 
 
 }
