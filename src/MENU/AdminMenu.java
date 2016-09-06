b9d394649fba022bab237274fb945254d94815fc
Martin Chn <cmaartinchan@gmail.com>
2016-09-06 15:16:44 +1000

    dwad

--- src/MENU/AdminMenu.java
+++ src/MENU/AdminMenu.java
@@ -3,12 +3,14 @@ package MENU;
 import java.util.*;
 import USERS.Admin;
 import Driver.Driver;
+import PROGRAMS.*;
 /**
  * Created by Martin on 1/09/2016.
  */
 public class AdminMenu {
 
     Driver driverClass = new Driver();
+    Program programClass = new Program();
 
     private void printHeader(){
 
@@ -20,7 +22,7 @@ public class AdminMenu {
     }
 
     private void menuOptions(){
-        System.out.println("\n1. Create Student Account");
+        System.out.println("\n1. Create Student");
         System.out.println("2. Create Program");
         System.out.println("3. Upload Enrolment(s)");
     }
@@ -29,21 +31,15 @@ public class AdminMenu {
     private void performChoices(int choice){
         switch (choice){
             case 1:
-               Scanner reader = new Scanner(System.in);
-            	System.out.println("How many student accounts would you like to add?");
-            	int numberOfAccounts = reader.nextInt();
-            	
-            	int i;
-            	for(i=0; i<numberOfAccounts; i++){
-            			String studentIDcreated = driverClass.createStudentLogin();        // makes sure it uses the studentID it created previously
-            			driverClass.createStudent(studentIDcreated);            			
-            			};
-            	break;
+                String studentIDcreated = driverClass.createStudentLogin();        // makes sure it uses the studentID it created previously
+                driverClass.createStudent(studentIDcreated);
+                break;
             case 2:
                 driverClass.createProgram();
                 break;
             case 3:
-                System.out.println("3");
+                programClass.loadProgram();
+                programClass.printPrograms();
                 break;
             default:
                 System.out.println("4");
@@ -59,7 +55,7 @@ public class AdminMenu {
         int choice = -1;
         Scanner input = new Scanner(System.in);
 
-        while( choice <0 || choice > 3){										// catches exception if not a number between 0 and 12.
+        while( choice <0 || choice > 4){										// catches exception if not a number between 0 and 12.
             try {
                 System.out.print("\nEnter Your Choice: ");
                 choice = Integer.parseInt(input.nextLine());
--- src/PROGRAMS/Program.java
+++ src/PROGRAMS/Program.java
@@ -4,25 +4,34 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Martin on 6/09/2016.
  */
 public class Program {
 
-    String[] program = new String[100];
+    ArrayList<ArrayList<String>> program = new ArrayList<ArrayList<String>>();
+    ArrayList<String> programDetails =  new ArrayList<String>();
+
+
     private int programCounter = 0;
 
-    public String loadProgram(){
+    public void loadProgram(){
         BufferedReader br;
         try {
-            br = new BufferedReader(new FileReader("loginDetails.txt"));
+            br = new BufferedReader(new FileReader("programs.txt"));
             try {
                 String x;
                 while ( (x = br.readLine()) != null ) {
                     // printing out each line in the file
-                    String programDetails[] = x.split(":", 5);
-                    program[programCounter] = programDetails;
+                    String programTxt[] = x.split(":", 5);
+                    for(int i = 0; i<5; i++){
+                        programDetails.add(programTxt[i]);
+                    }
+                    program.add(programDetails);
+
 
                 }
 
@@ -35,6 +44,20 @@ public class Program {
         }
 
 
+    }
+
+    public void printPrograms(){
+
+
+        for(int i = 0; i< 2; i++){
+            ArrayList hello = program.get(i);
+            for(int j = 0; j<5; j++){
+                System.out.println(hello.get(j));
+
+            }
+
+
+        }
 
 
     }
